08dd1ddc5a49142cf66b4555d4e398ec46f71f0b
programarivm <info@programarivm.com>
2018-07-20 23:47:46 +0100

    handling exceptions in src/Controller/Team/DeleteController.php

--- src/Controller/Team/DeleteController.php
+++ src/Controller/Team/DeleteController.php
@@ -6,6 +6,8 @@ use App\Entity\Team;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DeleteController extends AbstractController
 {
@@ -14,45 +16,45 @@ class DeleteController extends AbstractController
         try {
 
             if (!filter_var($request->get('id'), FILTER_VALIDATE_INT)) {
-                return new Response(
-                    json_encode([
-                        'message' => 'Invalid id.'
-                    ]),
-                    Response::HTTP_BAD_REQUEST,
-                    ['content-type' => 'application/json']
-                );
+                throw new BadRequestHttpException;
             }
 
             $em = $this->getDoctrine()->getManager();
             $team = $em->getRepository(Team::class)->find($request->get('id'));
 
             if (!$team) {
-                return new Response(
-                    json_encode([
-                        'message' => 'Team not found.'
-                    ]),
-                    Response::HTTP_NOT_FOUND,
-                    ['content-type' => 'application/json']
-                );
+                throw new NotFoundHttpException;
             }
 
             $em->remove($team);
             $em->flush();
 
             return new Response(
-                json_encode([
-                    'message' => 'Team successfully deleted.'
-                ]),
+                json_encode(['message' => 'Team successfully deleted.']),
                 Response::HTTP_OK,
+                ['content-type' => 'application/json']
+            );
+
+        } catch (BadRequestHttpException $e) {
+
+            return new Response(
+                json_encode(['message' => 'Bad request.']),
+                Response::HTTP_BAD_REQUEST,
+                ['content-type' => 'application/json']
+            );
+
+        } catch (NotFoundHttpException $e) {
+
+            return new Response(
+                json_encode(['message' => 'Not found.']),
+                Response::HTTP_NOT_FOUND,
                 ['content-type' => 'application/json']
             );
 
         } catch (\Exception $e) {
 
             return new Response(
-                json_encode([
-                    'message' => 'Internal server error.'
-                ]),
+                json_encode(['message' => 'Internal server error.']),
                 Response::HTTP_INTERNAL_SERVER_ERROR,
                 ['content-type' => 'application/json']
             );
