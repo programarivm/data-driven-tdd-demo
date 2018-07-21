7be88f92ef3012522eeda049a914d3fefe5e3954
programarivm <info@programarivm.com>
2018-07-21 10:36:53 +0100

    src/EventListener/ExceptionListener.php is added

--- src/Controller/Team/DeleteController.php
+++ src/Controller/Team/DeleteController.php
@@ -4,8 +4,8 @@ namespace App\Controller\Team;
 
 use App\Entity\Team;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -13,52 +13,24 @@ class DeleteController extends AbstractController
 {
     public function index(Request $request)
     {
-        try {
-
-            if (!filter_var($request->get('id'), FILTER_VALIDATE_INT)) {
-                throw new BadRequestHttpException;
-            }
-
-            $em = $this->getDoctrine()->getManager();
-            $team = $em->getRepository(Team::class)->find($request->get('id'));
-
-            if (!$team) {
-                throw new NotFoundHttpException;
-            }
-
-            $em->remove($team);
-            $em->flush();
-
-            return new Response(
-                json_encode(['message' => 'Team successfully deleted.']),
-                Response::HTTP_OK,
-                ['content-type' => 'application/json']
-            );
-
-        } catch (BadRequestHttpException $e) {
-
-            return new Response(
-                json_encode(['message' => 'Bad request.']),
-                Response::HTTP_BAD_REQUEST,
-                ['content-type' => 'application/json']
-            );
-
-        } catch (NotFoundHttpException $e) {
-
-            return new Response(
-                json_encode(['message' => 'Not found.']),
-                Response::HTTP_NOT_FOUND,
-                ['content-type' => 'application/json']
-            );
-
-        } catch (\Exception $e) {
-
-            return new Response(
-                json_encode(['message' => 'Internal server error.']),
-                Response::HTTP_INTERNAL_SERVER_ERROR,
-                ['content-type' => 'application/json']
-            );
-
+        if (!filter_var($request->get('id'), FILTER_VALIDATE_INT)) {
+            throw new BadRequestHttpException;
         }
+
+        $em = $this->getDoctrine()->getManager();
+        $team = $em->getRepository(Team::class)->find($request->get('id'));
+
+        if (!$team) {
+            throw new NotFoundHttpException;
+        }
+
+        $em->remove($team);
+        $em->flush();
+
+        return new Response(
+            json_encode(['message' => 'Team successfully deleted']),
+            Response::HTTP_OK,
+            ['content-type' => 'application/json']
+        );
     }
 }
---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ExceptionListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        $response = new Response();
+        $response->setStatusCode($exception->getStatusCode());
+        $response->headers->set('Content-Type', 'application/json');
+
+        if ($exception instanceof BadRequestHttpException) {
+            $content = json_encode([
+                'status' => $exception->getStatusCode(),
+                'message' => 'Bad Request'
+            ]);
+        } elseif ($exception instanceof NotFoundHttpException) {
+            $content = json_encode([
+                'status' => $exception->getStatusCode(),
+                'message' => 'Not Found'
+            ]);
+        } else {
+            $content = json_encode([
+                'status' => $exception->getStatusCode(),
+                'message' => 'Internal Server Error'
+            ]);
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $response->setContent($content);
+        
+        $event->setResponse($response);
+    }
+}
